e5e31d8d6a12f248ae6904f610eedb780aa9a3cf
qwer4tgyhjuki <93762035+qwer4tgyhjuki@users.noreply.github.com>
2021-11-05 10:26:21 +0200

    OP1

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+	int A, B, C;
+	cout << "Введіть число A " << endl;
+	cin >> A;
+	cout << "Введіть число B " << endl;
+	cin >> B;
+	cout << "Введіть число C " << endl;
+	cin >> C;
+
+	(((A > 0) && (B < 0)) && (C < 0)) ? (cout << "Тільки одне з чисел є позитивним ") : (cout << "Не тільки одне з чисел є позитивним ");
+	cout << "\nВведіть число A " << endl;
+	cin >> A;
+	cout << "Введіть число B " << endl;
+	cin >> B;
+	cout << "Введіть число C " << endl;
+	cin >> C;
+
+	(((A < 0) && (B > 0)) && (C < 0)) ? (cout << "Тільки одне з чисел є позитивним ") : (cout << "Не тільки одне з чисел є позитивним ");
+	cout << "\nВведіть число A " << endl;
+	cin >> A;
+	cout << "Введіть число B " << endl;
+	cin >> B;
+	cout << "Введіть число C " << endl;
+	cin >> C;
+
+	(((A < 0) && (B < 0)) && (C > 0)) ? (cout << "Тільки одне з чисел є позитивним ") : (cout << "Не тільки одне з чисел є позитивним ");
+}
